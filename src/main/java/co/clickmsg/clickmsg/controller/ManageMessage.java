c9dc52d4c5179f88daae8bb4afabe1b09556fd58
Alvin <tobteks@gmail.com>
2017-12-22 01:32:23 +0100

    completed manage message

---
+++ src/main/java/co/clickmsg/clickmsg/controller/ManageMessage.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.clickmsg.clickmsg.controller;
+
+import co.clickmsg.clickmsg.controller.util.Common;
+import co.clickmsg.clickmsg.domain.JsonMessage;
+import co.clickmsg.clickmsg.dto.JsonMessageDto;
+import co.clickmsg.clickmsg.service.JsonMessageService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import static java.util.Objects.isNull;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author tomide
+ */
+
+@Controller
+@RequestMapping("manage_messages")
+public class ManageMessage {
+    
+    @Autowired
+    private JsonMessageService jsonMessageService;
+    
+    private final ObjectMapper mapper = new ObjectMapper();
+    
+    @GetMapping
+    public String getManageMessage(Model model,Principal principal){
+        
+        String email =principal.getName();
+        
+        List<JsonMessage> messages = jsonMessageService.findByUserEmail(email);
+        
+        //System.out.println(messages.get(3).getId());
+        
+        List<JsonMessageDto> dto = new ArrayList<>();
+//        
+        for(JsonMessage msg : messages){
+            
+            try {
+                if(!isNull(msg)){
+                    Object test = mapper.readValue(msg.getMessage(), Object.class);
+                    
+                    dto.add(new JsonMessageDto(msg.getId(),msg.getName(),test));
+                         
+                }
+                
+            } catch (IOException ex) {
+                System.out.println(ex.getCause());
+            }
+            
+        }
+    
+      
+        
+        model.addAttribute("page", "manage_messages");
+        
+        model.addAttribute("messages", dto);
+        return Common.DASHBOARD;
+    }
+    
+}
